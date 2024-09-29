a5f98dc2f33c5683a7cbc1972d297d70d3274fe8
Muneer Girnari <muneeragirnari@gmail.com>
2024-09-30 01:24:30 +0530

    modified js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const now = new Date();
+let container = document.getElementById("timer");
+function updateDateTime(){
+    let timer = now.toLocaleTimeString();
+
+    let date = now.toLocaleDateString();
+
+    let string = date +" , " + timer;
+
+    container.innerText = `${date}, ${timer}`;
+}
+
+setInterval(updateDateTime, 1000);
